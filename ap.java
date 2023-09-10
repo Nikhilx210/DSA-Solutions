b7ae6d634c89dffd019440de98a78a7988bdacd7
Nikhilx210 <nikhilrathee736@gmail.com>
2023-09-10 22:42:49 +0530

    commit

--- ap.java
+++
@@ -1,7 +0,0 @@
-public class ap {
-    public static void main(String[] args) {
-        char ap='A';
-        Character.toLowerCase(ap);
-        System.out.println(Character.toLowerCase(ap));
-    }
-}
